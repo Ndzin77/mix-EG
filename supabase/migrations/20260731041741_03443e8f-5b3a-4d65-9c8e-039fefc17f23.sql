f83d9cd864f17c00f754c5c7f37ac31ec66598c7
Ndzin77 <fernandorochademoura@gmail.com>
2026-08-09 10:51:33 -0300

    .

--- supabase/migrations/20260731041741_03443e8f-5b3a-4d65-9c8e-039fefc17f23.sql
+++
@@ -1,2 +0,0 @@
-REVOKE ALL ON FUNCTION public.has_role(uuid, public.app_role) FROM PUBLIC, anon, authenticated;
-GRANT EXECUTE ON FUNCTION public.has_role(uuid, public.app_role) TO service_role;
--- supabase/migrations/20260807042306_bd5b2387-2c55-45f6-8ff2-58ef5f09c538.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE public.store_settings
-  ADD COLUMN IF NOT EXISTS recibo_config jsonb NOT NULL DEFAULT '{}'::jsonb;
